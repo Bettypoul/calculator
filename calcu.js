d98f5e217c374b74dc75e5a248bc57bc40dd1caa
Bettypoul <bettybabu075@gmail.com>
2024-05-11 00:42:39 +0530

    calculator

---
+++ calcu.js
@@ -0,0 +1,25 @@
+function display(num){
+   result.value+=num
+}
+
+
+function clearall(){
+    result.value=''    //or    result.value=0
+}
+
+function back(){
+    result.value=result.value.slice(0,-1)
+}
+
+function equal(){
+    try{
+        result.value=eval(result.value)
+    }
+    catch{
+        result.value='Error'
+        setTimeout(()=>{
+          result.value=''  
+        },1000)
+    }
+   
+}
